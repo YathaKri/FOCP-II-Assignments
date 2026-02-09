c75deb312fd49aa1d2fc05c8aa5ce6798aa2c4cf
Yatharth Pandey <yatharthpandey700@gmail.com>
2026-02-09 08:49:17 +0530

    removed test

--- lab manual/q33.cpp
+++ lab manual/q33.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
     int marks[3][5];
 
     cout << "Enter marks for 3 students (5 subjects each):\n";
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         cout << "Student " << i + 1 << ":\n";
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 5; j++)
+        {
             cout << "  Subject " << j + 1 << ": ";
             cin >> marks[i][j];
         }
@@ -16,6 +19,6 @@ int main() {
 
     cout << "\nMarks in 2nd subject of 1st student: " << marks[0][1] << endl;
     cout << "Marks in 5th subject of 3rd student: " << marks[2][4] << endl;
-//test
+
     return 0;
 }
